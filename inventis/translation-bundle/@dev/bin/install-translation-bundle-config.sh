62f50c1f8ea53ccee041bec6f77640a16bd52d15
Joren Martens <joren@inventis.be>
2018-11-15 11:15:19 +0100

    new recipes

--- inventis/translation-bundle/@dev/bin/install-translation-bundle-config.sh
+++
@@ -1,5 +0,0 @@
-resource='@InventisTranslationBundle/Resources/config/routing.yaml'
-dir='./config/routes.yaml'
-if ! grep -q ${resource} ${dir}; then
-  echo -e "\ninventis_translation_bundle:\n\t- { resource: "${resource}" }" >> ${dir}
-fi
